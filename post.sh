43a1115349a8f24f2e2d370fd04e38337e1b1617
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-03-07 14:21:27 +0100

    add multiple lines submit

--- post.sh
+++ post.sh
@@ -2,6 +2,9 @@
 
 # https://jwt.io/
 token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoid2ViX2Fub24ifQ.fVeM01NuFk7rKb8m9oVRyxZziAQbD72bdFcsKcQk-kA"
-curl "http://localhost/predictions" -X POST -H "Authorization: Bearer $token" -H "Prefer: return=minimal" -H "Content-Type: application/json" -d '{"prediction": 5, "team": "data wizards"}'
+curl "http://datathon.intelliris.net/predictions"  \
+  -X POST -H "Authorization: Bearer $token" -H "Prefer: return=minimal"  \
+  -H "Content-Type: application/json" \
+  -d '[{"team_id": '"$RANDOM"', "user_id": '"$RANDOM"', "datediff": '"$RANDOM"', "quantity": '"$RANDOM"', "correct": "FALSE"}, {"team_id": '"$RANDOM"', "user_id": '"$RANDOM"', "datediff": '"$RANDOM"', "quantity": '"$RANDOM"', "correct": "FALSE"}]'
 
 exit 0
