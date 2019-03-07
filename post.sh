fe7544ac46a08e463787ee22a64b55da2e4b4ef3
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-03-07 14:51:34 +0100

    remove correct column from json packet

--- post.sh
+++ post.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+thehost="$1"
+
 # https://jwt.io/
 token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoid2ViX2Fub24ifQ.fVeM01NuFk7rKb8m9oVRyxZziAQbD72bdFcsKcQk-kA"
-curl "http://datathon.intelliris.net/predictions"  \
+curl "http://$thehost/predictions"  \
   -X POST -H "Authorization: Bearer $token" -H "Prefer: return=minimal"  \
   -H "Content-Type: application/json" \
-  -d '[{"team_id": '"$RANDOM"', "user_id": '"$RANDOM"', "datediff": '"$RANDOM"', "quantity": '"$RANDOM"', "correct": "FALSE"}, {"team_id": '"$RANDOM"', "user_id": '"$RANDOM"', "datediff": '"$RANDOM"', "quantity": '"$RANDOM"', "correct": "FALSE"}]'
+  -d '[{"team_id": '"$RANDOM"', "user_id": '"$RANDOM"', "datediff": '"$RANDOM"', "quantity": '"$RANDOM"'}, {"team_id": '"$RANDOM"', "user_id": '"$RANDOM"', "datediff": '"$RANDOM"', "quantity": '"$RANDOM"'}]'
 
 exit 0
