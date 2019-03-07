10e8cd2455d66f12b1320d67038d315bfba8f463
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-03-07 20:32:32 +0100

    add conversation with the API

--- clients/client.py
+++ clients/client.py
@@ -15,18 +15,60 @@ logger = logging.getLogger(__name__)
 
 import pandas as pd
 import requests as re
+import datetime as dt
+import json
+
+protocol = 'http://'
+host = 'localhost'
+
+team = 'lolos'
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoid2ViX2Fub24ifQ.fVeM01NuFk7rKb8m9oVRyxZziAQbD72bdFcsKcQk-kA"
 
 df = pd.read_csv('data.csv')
+if df.empty():
+    quit()
+# add unique usernum constrain!
 
-payload = df.to_json(orient='records')
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoid2ViX2Fub24ifQ.fVeM01NuFk7rKb8m9oVRyxZziAQbD72bdFcsKcQk-kA"
+# get team id
+endpoint = 'teams'
+condition = 'name=eq.{}'.format(team)
+url = protocol + host + '/' + endpoint + '?' + condition
+r = re.get(url)
+if len(r.json()) == 1: # check r.json() is list of length 1
+    team_id = r.json()[0]['id']
+else:
+    print("error! more than one team with that name???")
+    quit()
+
+endpoint = 'submissions'
+url = protocol + host + '/' + endpoint
 headers = {
     "Content-Type": "application/json",
-    "Prefer": "return=minimal",
     "Authorization": "Bearer {}".format(token),
+    "Prefer": "return=representation",
 }
+payload = json.dumps({
+    'team_id': team_id,
+    'timestamp': dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+})
+r = re.post(url, headers=headers, data=payload)
+if len(r.json()) == 1:
+    submission_id = r.json()[0]['id']
+else:
+    print("error! more than one submission posted at the same time???")
+    quit()
+
+quit()
+
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer {}".format(token),
+    "Prefer": "return=minimal",
+}
+payload = df.to_json(orient='records')
 url='http://localhost/predictions'
 
-# response = re.get(url)
-response = re.post(url, data=payload, headers=headers)
-print(response.status_code)
+# r = re.get(url)
+r = re.post(url, data=payload, headers=headers)
+print(r.status_code)
