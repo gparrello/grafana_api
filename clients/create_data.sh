2a692a8898722f1c622c6c011b5950df61beb126
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-03-07 20:53:55 +0100

    remove column

--- clients/create_data.sh
+++ clients/create_data.sh
@@ -2,10 +2,10 @@
 
 n=100000
 i=1
-echo "submission_id,usernum,datediff,quantity" > data.csv
+echo "usernum,datediff,quantity" > data.csv
 while [ $i -le $n ]
 do
-  echo "$RANDOM,$RANDOM,$RANDOM,$RANDOM" >> data.csv
+  echo "$RANDOM,$RANDOM,$RANDOM" >> data.csv
   i=$((i+1))
 done
 
